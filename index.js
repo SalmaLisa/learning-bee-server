8fc37cda7e4e638a91fe15db19397d0f0253ba96
Salmalisa <salmalisa7342@gmail.com>
2022-10-26 12:42:46 +0600

    cors added and test api deleted

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 const courses = require('./data/courses.json')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+
 app.get('/courses', (req, res) => {
   res.send(courses)
 })
