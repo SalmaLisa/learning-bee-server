a772f1ecc0b0457f2ae20bdca26c3c2c76e58329
Salmalisa <salmalisa7342@gmail.com>
2022-10-26 12:33:58 +0600

    single coures api is ready

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/courses', (req, res) => {
   res.send(courses)
 })
 
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const selected_course = courses.find(course => course._id === id)
+  res.send(selected_course)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
